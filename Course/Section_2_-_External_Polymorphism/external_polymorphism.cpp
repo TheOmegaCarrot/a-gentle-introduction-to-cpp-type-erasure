9714d149f65fb845cf5e819a3d26e6a3cef66301
TheOmegaCarrot <theomegacarrot@gmail.com>
2023-10-28 15:56:15 -0400

    basic external polymorphism example

--- Course/Section_2_-_External_Polymorphism/external_polymorphism.cpp
+++ Course/Section_2_-_External_Polymorphism/external_polymorphism.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 
+// Here is our existing inheritance tree
 class Animal
 {
 
 public:
 
   Animal() = default;
-  Animal(const Animal&) = delete;
-  Animal(Animal&&) = delete;
-  Animal& operator=(const Animal&) = delete;
-  Animal& operator=(Animal&&) = delete;
+  Animal(const Animal&) = default;
+  Animal(Animal&&) = default;
+  Animal& operator=(const Animal&) = default;
+  Animal& operator=(Animal&&) = default;
   virtual ~Animal() = default;
 
   // here is our interface
@@ -22,10 +25,10 @@ class Cat : public Animal
 public:
 
   Cat() = default;
-  Cat(const Cat&) = delete;
-  Cat(Cat&&) = delete;
-  Cat& operator=(const Cat&) = delete;
-  Cat& operator=(Cat&&) = delete;
+  Cat(const Cat&) = default;
+  Cat(Cat&&) = default;
+  Cat& operator=(const Cat&) = default;
+  Cat& operator=(Cat&&) = default;
   ~Cat() override = default;
 
   void speak() const override
@@ -40,10 +43,10 @@ class Dog : public Animal
 public:
 
   Dog() = default;
-  Dog(const Dog&) = delete;
-  Dog(Dog&&) = delete;
-  Dog& operator=(const Dog&) = delete;
-  Dog& operator=(Dog&&) = delete;
+  Dog(const Dog&) = default;
+  Dog(Dog&&) = default;
+  Dog& operator=(const Dog&) = default;
+  Dog& operator=(Dog&&) = default;
   ~Dog() override = default;
 
   void speak() const override
@@ -58,10 +61,10 @@ class Horse : public Animal
 public:
 
   Horse() = default;
-  Horse(const Horse&) = delete;
-  Horse(Horse&&) = delete;
-  Horse& operator=(const Horse&) = delete;
-  Horse& operator=(Horse&&) = delete;
+  Horse(const Horse&) = default;
+  Horse(Horse&&) = default;
+  Horse& operator=(const Horse&) = default;
+  Horse& operator=(Horse&&) = default;
   ~Horse() override = default;
 
   void speak() const override
@@ -69,3 +72,77 @@ public:
     std::cout << "Neigh!\n";
   }
 };
+
+// and here is our Sheep that doesn't inherit from `Animal`
+
+class Sheep
+{
+
+public:
+
+  Sheep() = default;
+  Sheep(const Sheep&) = default;
+  Sheep(Sheep&&) = default;
+  Sheep& operator=(const Sheep&) = default;
+  Sheep& operator=(Sheep&&) = default;
+  ~Sheep() = default;
+
+  void speak() const
+  {
+    std::cout << "Baa!\n";
+  }
+};
+
+// but we can make a SheepWrapper that does!
+class SheepWrapper : public Animal
+{
+private:
+
+  // we hold onto a real sheep
+  Sheep wrappedSheep;
+
+public:
+
+  SheepWrapper() = default;
+
+  // we make a constructor that takes a sheep
+  explicit SheepWrapper(Sheep sheep)
+      : wrappedSheep {sheep}  // in real code,
+  // you should move or forward in the sheep
+  // but this is just an introduction
+  { }
+
+  SheepWrapper(const SheepWrapper&) = delete;
+  SheepWrapper(SheepWrapper&&) = delete;
+  SheepWrapper& operator=(const SheepWrapper&) = delete;
+  SheepWrapper& operator=(SheepWrapper&&) = delete;
+  ~SheepWrapper() override = default;
+
+  // and we just implement the Animal interface by calling
+  // the member functions of the real Sheep!
+  void speak() const override
+  {
+    wrappedSheep.speak();
+  }
+};
+
+int main()
+{
+  std::vector<Animal*> myAnimals;
+
+  myAnimals.emplace_back(new Cat {});
+  myAnimals.emplace_back(new Dog {});
+  myAnimals.emplace_back(new Horse {});
+
+  // This would not work, because Sheep does not inherit from Animal
+  // myAnimals.emplace_back(new Sheep {});
+
+  // but this does work!
+  // Because SheepWrapper does inherit from Animal!
+  myAnimals.emplace_back(new SheepWrapper {});
+
+  for ( Animal* animal : myAnimals ) {
+    animal->speak();
+    delete animal;
+  }
+}
