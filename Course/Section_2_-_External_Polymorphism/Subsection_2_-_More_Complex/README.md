e7d20e7bb8ef45e2e01563fc37a2d7292a0aceee
TheOmegaCarrot <theomegacarrot@gmail.com>
2023-11-13 16:38:03 -0500

    external polymorphism example with parameters and return value

--- Course/Section_2_-_External_Polymorphism/Subsection_2_-_More_Complex/README.md
+++ Course/Section_2_-_External_Polymorphism/Subsection_2_-_More_Complex/README.md
@@ -3,3 +3,7 @@
 Excellent question! We can just pass those parameters to the wrapped `Sheep`, and return what it returns!
 
 To illustrate this, let's add another member function to our `Animal` interface.
+`bool can_eat(std::string_view)` is a pretty simple function, but it works as an example.
+
+The biggest hurdle here is avoiding expensive copying of parameters.
+You may want to take parameters by reference in many cases, but in this example, `std::string_view` is very cheap to copy.
---
+++ Course/Section_2_-_External_Polymorphism/Subsection_2_-_More_Complex/more_complex.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <memory>
+#include <string_view>
+#include <vector>
+
+// Here is our existing inheritance tree
+class Animal
+{
+
+public:
+
+  Animal() = default;
+  Animal(const Animal&) = default;
+  Animal(Animal&&) = default;
+  Animal& operator=(const Animal&) = default;
+  Animal& operator=(Animal&&) = default;
+  virtual ~Animal() = default;
+
+  // here is our interface
+  virtual void speak() const = 0;
+  virtual bool can_eat(std::string_view) const = 0;
+};
+
+class Cat : public Animal
+{
+
+public:
+
+  Cat() = default;
+  Cat(const Cat&) = default;
+  Cat(Cat&&) = default;
+  Cat& operator=(const Cat&) = default;
+  Cat& operator=(Cat&&) = default;
+  ~Cat() override = default;
+
+  void speak() const override
+  {
+    std::cout << "Meow!\n";
+  }
+
+  bool can_eat(std::string_view food) const override
+  {
+    return food == "cat food";
+  }
+};
+
+class Dog : public Animal
+{
+
+public:
+
+  Dog() = default;
+  Dog(const Dog&) = default;
+  Dog(Dog&&) = default;
+  Dog& operator=(const Dog&) = default;
+  Dog& operator=(Dog&&) = default;
+  ~Dog() override = default;
+
+  void speak() const override
+  {
+    std::cout << "Woof!\n";
+  }
+
+  bool can_eat(std::string_view food) const override
+  {
+    return food == "dog food";
+  }
+};
+
+class Horse : public Animal
+{
+
+public:
+
+  Horse() = default;
+  Horse(const Horse&) = default;
+  Horse(Horse&&) = default;
+  Horse& operator=(const Horse&) = default;
+  Horse& operator=(Horse&&) = default;
+  ~Horse() override = default;
+
+  void speak() const override
+  {
+    std::cout << "Neigh!\n";
+  }
+
+  bool can_eat(std::string_view food) const override
+  {
+    return food == "hay";
+  }
+};
+
+// and here is our Sheep that doesn't inherit from `Animal`
+
+class Sheep
+{
+
+public:
+
+  Sheep() = default;
+  Sheep(const Sheep&) = default;
+  Sheep(Sheep&&) = default;
+  Sheep& operator=(const Sheep&) = default;
+  Sheep& operator=(Sheep&&) = default;
+  ~Sheep() = default;
+
+  void speak() const
+  {
+    std::cout << "Baa!\n";
+  }
+
+  bool can_eat(std::string_view food) const
+  {
+    return food == "grass";
+  }
+};
+
+// but we can make a SheepWrapper that does!
+class SheepWrapper : public Animal
+{
+private:
+
+  // we hold onto a real sheep
+  Sheep wrappedSheep;
+
+public:
+
+  SheepWrapper() = default;
+
+  // we make a constructor that takes a sheep
+  explicit SheepWrapper(Sheep sheep)
+      : wrappedSheep {sheep}  // in real code,
+  // you should move or forward in the sheep
+  // but this is just an introduction
+  { }
+
+  SheepWrapper(const SheepWrapper&) = delete;
+  SheepWrapper(SheepWrapper&&) = delete;
+  SheepWrapper& operator=(const SheepWrapper&) = delete;
+  SheepWrapper& operator=(SheepWrapper&&) = delete;
+  ~SheepWrapper() override = default;
+
+  // and we just implement the Animal interface by calling
+  // the member functions of the real Sheep!
+  void speak() const override
+  {
+    wrappedSheep.speak();
+  }
+
+  bool can_eat(std::string_view food) const override
+  {
+    return wrappedSheep.can_eat(food);
+  }
+};
+
+int main()
+{
+  std::vector<Animal*> myAnimals;
+
+  myAnimals.emplace_back(new Cat {});
+  myAnimals.emplace_back(new Dog {});
+  myAnimals.emplace_back(new Horse {});
+
+  // This would not work, because Sheep does not inherit from Animal
+  // myAnimals.emplace_back(new Sheep {});
+
+  // but this does work!
+  // Because SheepWrapper does inherit from Animal!
+  myAnimals.emplace_back(new SheepWrapper {});
+
+  for ( Animal* animal : myAnimals ) {
+    animal->speak();
+    std::cout << "Can this animal eat grass? " << std::boolalpha
+              << animal->can_eat("grass") << '\n';
+    delete animal;
+  }
+}
